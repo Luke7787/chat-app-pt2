e114a7ebb0e61d63a28d94d0338e7c6f3a6fbd88
Luke Zhuang <Lukezhuang@yahoo.com>
2026-08-23 21:05:07 -0700

    updated port to 5001, and added test get route to root

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const app = express();
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.get("/", (req, res) => {
+  // root route http://localhost:5001/
+  res.send("Hello, World!");
+});
+
+app.listen(5001, () => {
+  console.log("Server is running on port 5001");
 });
