a85f3939170d9d90e3a3f7f446752c651b0ac1cd
Luke Zhuang <Lukezhuang@yahoo.com>
2026-08-25 17:39:24 -0700

    created signup & login endpoint

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,14 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
+app.get("/api/auth/signup", (req, res) => {
+  console.log("signup route");
+});
+
+app.get("/api/auth/login", (req, res) => {
+  console.log("login route");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
