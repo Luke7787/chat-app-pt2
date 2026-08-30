01881fd47b0792dfe007238b1afd3b9a3f7f4adf
Luke Zhuang <Lukezhuang@yahoo.com>
2026-08-29 18:03:10 -0700

    set up auth.routes.js

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -2,16 +2,10 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/signup", (req, res) => {
-  res.send("Signup route");
-});
+router.get("/signup");
 
-router.get("/login", (req, res) => {
-  res.send("Login route");
-});
+router.get("/login");
 
-router.get("/logout", (req, res) => {
-  res.send("Logout route");
-});
+router.get("/logout");
 
 export default router;
