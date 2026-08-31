160c81dd7d2ad4d4a1c85113b225ff353efe5854
Luke Zhuang <Lukezhuang@yahoo.com>
2026-08-31 10:14:42 -0700

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,18 @@ import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-
-dotenv.config();
 const PORT = process.env.PORT || 5001;
 
-app.get("/", (req, res) => {
-  // root route http://localhost:5001/
-  res.send("Hello, World!");
-});
+dotenv.config();
+
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+
+// app.get("/", (req, res) => {
+// root route http://localhost:5001/
+//   res.send("Hello, World!");
+// });
 
 app.listen(PORT, () => {
   connectToMongoDB();
