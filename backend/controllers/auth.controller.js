9ba42ddb6441ed8d02e3711a35daeab65145c1bc
Luke Zhuang <Lukezhuang@yahoo.com>
2026-09-02 17:38:20 -0700

    started to work on signup controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,20 @@
 export const signup = async (req, res) => {
   try {
     const { fullName, username, password, confirmPassword, gender } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Passwords do not match" });
+    }
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+      return res.status(400).json({ error: "Username already exists" });
+    }
+
+    // HASH PASSWORD HERE
+
+    // https://avatar-placeholder.iran.liara.run
   } catch (error) {}
 };
 
